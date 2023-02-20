fd3a7910920b02aa8e9bea3c6411290bb2962903
Nachiket <nachimak28@gmail.com>
2023-02-20 18:21:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FastAPI Celery prototype
+
+This is an extremely basic fastapi prototype with celery task workers using redis as the message broker.
---
+++ main.py
@@ -0,0 +1,30 @@
+from celery.result import AsyncResult
+from fastapi import Body, FastAPI, Form, Request
+from fastapi.responses import JSONResponse
+from worker import create_task
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def home(request: Request):
+    return JSONResponse({"message": "Hello world"})
+
+
+@app.post("/tasks", status_code=201)
+def run_task(payload = Body(...)):
+    task_type = payload["type"]
+    task = create_task.delay(int(task_type))
+    return JSONResponse({"task_id": task.id})
+
+
+@app.get("/tasks/{task_id}")
+def get_status(task_id):
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_id": task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result
+    }
+    return JSONResponse(result)
